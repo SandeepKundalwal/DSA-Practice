6be449d7c983ef611987f09b61c96d75a3df80f8
SKundalwal <sandeepkundalwal96@gmail.com>
2023-07-26 22:57:40 +0530

    adding InvertBinaryTree

---
+++ Leetcode/InvertBinaryTree/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        invertTree(null);
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
