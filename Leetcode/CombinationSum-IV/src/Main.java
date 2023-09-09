09cb93401758353a3f9a0718e4d0e2cfb0718dae
SKundalwal <sandeepkundalwal96@gmail.com>
2023-09-09 11:05:19 +0530

    adding CombinationSum-IV

---
+++ Leetcode/CombinationSum-IV/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int target = 4;
+        int nums[] = {1, 2, 3};
+        System.out.println(combinationSum4(nums, target));
+    }
+
+    public static int combinationSum4(int[] nums, int target) {
+        int MEMO[] = new int[1001];
+        Arrays.fill(MEMO, -1);
+
+        return f(0, 0, target, nums, MEMO);
+    }
+
+    public static int f(int idx, int sum, int target, int nums[], int MEMO[]) {
+        if (sum == target) {
+            return 1;
+        }
+
+        if (idx > nums.length || sum > target) {
+            return 0;
+        }
+
+        if (MEMO[sum] != -1) {
+            return MEMO[sum];
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum + nums[i] > target) continue;
+            cnt += f(i, sum + nums[i], target, nums, MEMO);
+        }
+
+        return MEMO[sum] = cnt;
+    }
+}
