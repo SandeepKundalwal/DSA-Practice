6d8e8a3be3e61e7c6f7ab2b3dfc06363adbc106a
SKundalwal <sandeepkundalwal96@gmail.com>
2023-09-29 15:56:58 +0530

    optimizing MonotonicArray to Single Pass

--- Leetcode/MonotonicArray/src/Main.java
+++ Leetcode/MonotonicArray/src/Main.java
@@ -1,30 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int nums[] = {1,2,2,3,4,5,5};
-        System.out.println("isMonotonic Ascending/Descending: " + isMonotonic(nums));
+        System.out.println("isMonotonic: " + isMonotonic(nums));
     }
 
     public static boolean isMonotonic(int[] nums) {
-        boolean isMonotonic = true;
-        // Ascending Monotonic;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i - 1] > nums[i]){
-                isMonotonic = false;
-            }
-        }
-
-        if(isMonotonic){
+        if(nums.length <= 2){
             return true;
         }
 
-        isMonotonic = true;
-        // Descending Monotonic
+        boolean isAscending = true, isDescending = true;
         for(int i = 1; i < nums.length; i++){
-            if(nums[i - 1] < nums[i]){
-                isMonotonic = false;
-            }
+            if(nums[i - 1] < nums[i]) isDescending = false;
+            if(nums[i - 1] > nums[i]) isAscending = false;
         }
-
-        return isMonotonic;
+        return isAscending || isDescending;
     }
 }
