687d6bef3303cab980233f7ac34b1ce45a440ad8
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2024-01-10 11:08:25 +0530

    adding AmountOfTimeForBinaryTreeToBeInfected

---
+++ Leetcode/AmountOfTimeForBinaryTreeToBeInfected/src/Main.java
@@ -0,0 +1,75 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Need to Make Tree Structure.");
+    }
+
+    public int amountOfTime(TreeNode root, int start) {
+        if(root == null) return 0;
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        generateGraph(root, graph);
+
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> alreadyInfected = new HashSet<>();
+        queue.offer(start);
+
+        int minutes = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            boolean newlyInfected = false;
+            while(size-- > 0){
+                int node = queue.remove();
+                if(!alreadyInfected.contains(node)){
+                    alreadyInfected.add(node);
+                    List<Integer> neighbours = graph.get(node);
+                    for(Integer nodes : neighbours){
+                        if(!alreadyInfected.contains(nodes)){
+                            queue.offer(nodes);
+                            newlyInfected = true;
+                        }
+                    }
+                }
+            }
+            if(newlyInfected) minutes++;
+        }
+
+        return minutes;
+    }
+
+    public void generateGraph(TreeNode root, Map<Integer, List<Integer>> graph){
+        if(root == null){
+            return;
+        }
+
+        if(!graph.containsKey(root.val)){
+            graph.put(root.val, new ArrayList<>());
+        }
+
+        if(root.left != null){
+            if(!graph.containsKey(root.left.val)){
+                graph.put(root.left.val, new ArrayList<>());
+            }
+            graph.get(root.val).add(root.left.val);
+            graph.get(root.left.val).add(root.val);
+            generateGraph(root.left, graph);
+        }
+
+        if(root.right != null){
+            if(!graph.containsKey(root.right.val)){
+                graph.put(root.right.val, new ArrayList<>());
+            }
+            graph.get(root.val).add(root.right.val);
+            graph.get(root.right.val).add(root.val);
+            generateGraph(root.right, graph);
+        }
+    }
+}
