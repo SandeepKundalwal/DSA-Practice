86340bb3c38845d7d43d7b635fcc0e25acd0f6dd
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2024-01-10 12:49:20 +0530

    adding MoveZeroes

---
+++ Leetcode/MoveZeroes/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int nums[] = {1,0,3,0,13,0,0};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int idx = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[idx++] = nums[i];
+            }
+        }
+
+        for(int i = idx; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
