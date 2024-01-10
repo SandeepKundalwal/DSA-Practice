be50fb3f8b458350145ff37bee6d41a660196d5f
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2024-01-10 15:38:35 +0530

    adding SortColors

---
+++ Leetcode/SortColors/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int nums[] = {2,0,2,1,1,0};
+        System.out.println(Arrays.toString(sortColors(nums)));
+    }
+
+    // Dutch National Flag Algorithm
+    public static int[] sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(low, mid, nums);
+                low++;
+                mid++;
+            } else if(nums[mid] == 1){
+                mid++;
+            } else {
+                swap(mid, high, nums);
+                high--;
+            }
+        }
+
+        return nums;
+    }
+
+    public static void swap(int i, int j, int nums[]){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
