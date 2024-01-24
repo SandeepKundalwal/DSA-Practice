6af910bbfabb78a35f5fdbbf4f8cdd1e0b5c028f
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2024-01-24 15:14:06 +0530

    updating Optimized Code for Pseudo-PalindromicPathsInABinaryTree

--- Leetcode/Pseudo-PalindromicPathsInABinaryTree/src/Main.java
+++ Leetcode/Pseudo-PalindromicPathsInABinaryTree/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         TreeNode root = TreeNode.generateTree(new Integer[]{2,3,1,3,1,null,1});
         System.out.println(pseudoPalindromicPaths(root));
+        System.out.println(pseudoPalindromicPathsOptimized(root));
     }
 
     public static int pseudoPalindromicPaths (TreeNode root) {
@@ -35,5 +36,30 @@ public class Main {
             }
         }
         return oddCount <= 1;
+    }
+
+/********************************************************************************************************/
+    static int cnt;
+    public static int pseudoPalindromicPathsOptimized(TreeNode root) {
+        cnt = 0;
+        fOptimized(root, 0);
+        return cnt;
+    }
+
+    public static void fOptimized(TreeNode root, int seen){
+        if(root == null){
+            return;
+        }
+
+        seen ^= (1 << root.val);
+        if(root.left == null && root.right == null){
+            if(Integer.bitCount(seen) <= 1){
+                cnt++;
+            }
+        }
+
+        fOptimized(root.left, seen);
+        fOptimized(root.right, seen);
+        seen ^= (1 << root.val);
     }
 }
