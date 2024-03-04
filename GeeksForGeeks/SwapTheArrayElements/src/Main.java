e15df1f383d77536aa2d1c2c969737e174a8bd2c
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2024-03-04 11:11:23 +0530

    uploading POTD:04/03/24

---
+++ GeeksForGeeks/SwapTheArrayElements/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        int arr[] = {1,2,3};
+        swapElements(arr, n);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void swapElements(int[] arr, int n) {
+        for(int i = 0; i < n - 2; i++){
+            int temp = arr[i];
+            arr[i] = arr[i + 2];
+            arr[i + 2] = temp;
+        }
+    }
+}
