594d79de4e2c83286580049826da4a2418cead39
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2024-04-01 21:30:23 +0530

    uploading DailyProblem:01/04/24

---
+++ Leetcode/LengthOfLastWord/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "   fly me   to   the moon  ";
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        int len = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') len++;
+            else if (len != 0) break;
+        }
+        return len;
+    }
+}
