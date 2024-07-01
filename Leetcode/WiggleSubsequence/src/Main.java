54ab0798c472dccf149bc8b1add24e271e9a2df1
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2024-07-01 20:30:54 +0530

    uploading Optimzed Solution for WiggleSubsequence

--- Leetcode/WiggleSubsequence/src/Main.java
+++ Leetcode/WiggleSubsequence/src/Main.java
@@ -3,9 +3,60 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int nums[] = {1,17,5,10,13,15,10,5,16,8};
+        System.out.println(wiggleMaxLength(nums));
         System.out.println(wiggleMaxLengthON2(nums));
     }
 
+    /*
+    Linear Time Solution
+     */
+    public static int wiggleMaxLength(int[] nums) {
+        int n = nums.length;
+
+        int peak = 0;
+        int wiggleSubsequence = 0;
+        for(int i = 0; i < n - 1;){
+
+            int j = i + 1;
+            while(j < n && nums[i] == nums[j]){
+                i++;
+                j++;
+            }
+
+            int originalI = i;
+
+            int k = i + 1;
+            while(k < n && nums[i] < nums[k]){
+                i++;
+                k++;
+            }
+
+            if(originalI + 1 != k && (peak == 0 || peak == 1)){
+                peak = -1;
+                wiggleSubsequence++;
+            }
+
+            originalI = i;
+
+            int l = i + 1;
+            while(l < n && nums[i] > nums[l]){
+                i++;
+                l++;
+            }
+
+            if(originalI + 1 != l && (peak == 0 || peak == -1)){
+                peak = 1;
+                wiggleSubsequence++;
+            }
+
+        }
+
+        return wiggleSubsequence + 1;
+    }
+
+    /*
+     Recursion + Memoization: O(n^2)
+     */
     public static int wiggleMaxLengthON2(int[] nums) {
         int n = nums.length;
 
