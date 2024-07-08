f761d31af028270354647bc29339d6773b5f9114
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2024-07-08 12:13:02 +0530

    updating to Constant Space Solution

--- Leetcode/FindTheWinnerOfTheCircularGame/src/Main.java
+++ Leetcode/FindTheWinnerOfTheCircularGame/src/Main.java
@@ -6,15 +6,27 @@ public class Main {
         int n = 5;
         int k = 2;
         System.out.println("Linear Space: " + findTheWinnerLinearSpace(n, k));
+        System.out.println("Recursion Stack Space: " + (findTheWinnerRecursionStack(n , k) + 1));
         System.out.println("Constant Space: " + (findTheWinner(n , k) + 1));
     }
 
-    static int findTheWinner(int n, int k){
+    public static int findTheWinner(int n, int k) {
+        int result = 0;
+        for(int friend = 1; friend <= n; friend++){
+            result = (result + k) % friend;
+        }
+        return result + 1;
+    }
+
+    /*
+    recursion stack space
+     */
+    static int findTheWinnerRecursionStack(int n, int k){
         if(n == 1){
             return 0;
         }
 
-        return (findTheWinner(n - 1, k) + k) % n;
+        return (findTheWinnerRecursionStack(n - 1, k) + k) % n;
     }
 
     // Linear Space
