4eb8928f43e9f35b72b991d46e9e8c3d1a278f3a
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2024-07-13 12:55:08 +0530

    uploading Optimized Space Solution for MaximumScoreFromRemovingSubstrings

--- Leetcode/MaximumScoreFromRemovingSubstrings/src/Main.java
+++ Leetcode/MaximumScoreFromRemovingSubstrings/src/Main.java
@@ -6,14 +6,44 @@ public class Main {
         int x = 1;
         int y = 100;
         System.out.println(maximumGain(s, x, y));
+        System.out.println("Using Stack: " + maximumGainUsingStack(s, x, y));
     }
 
+    /*
+    Better Space Complexity
+     */
     public static int maximumGain(String s, int x, int y) {
-        return x > y ? f(s, x, y, 'a', 'b') : f(s, y, x, 'b', 'a');
+        StringBuilder newS = new StringBuilder(s);
 
+        return x > y ?  f(newS, x, new char[]{'a', 'b'}) + f(newS, y, new char[]{'b', 'a'}) :
+                f(newS, y, new char[]{'b', 'a'}) + f(newS, x, new char[]{'a', 'b'});
     }
 
-    public static int f(String s, int max, int min, char first, char second){
+    public static int f(StringBuilder newS, int point, char neededS[]){
+        int n = newS.length();
+
+        int score = 0, wIdx = 0;
+        for(int rIdx = 0; rIdx < n; rIdx++){
+            newS.setCharAt(wIdx++, newS.charAt(rIdx));
+
+            if(wIdx > 1 && newS.charAt(wIdx - 2) == neededS[0] && newS.charAt(wIdx - 1) == neededS[1]){
+                wIdx -= 2;
+                score += point;
+            }
+        }
+
+        newS.setLength(wIdx);
+        return score;
+    }
+
+    /*
+    Using Stack
+     */
+    public static int maximumGainUsingStack(String s, int x, int y) {
+        return x > y ? fUsingStack(s, x, y, 'a', 'b') : fUsingStack(s, y, x, 'b', 'a');
+    }
+
+    public static int fUsingStack(String s, int max, int min, char first, char second){
         int score = 0;
 
         Stack<Character> stk = new Stack<>();
