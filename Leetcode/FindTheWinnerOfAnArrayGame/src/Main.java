0efd88ff4290c817ef87435dd820d367759d93ab
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2024-07-20 20:45:55 +0530

    uploading FindTheWinnerOfAnArrayGame

---
+++ Leetcode/FindTheWinnerOfAnArrayGame/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {18,15,20};
+        int k = 2;
+        System.out.println(getWinner(arr, k));
+    }
+
+    public static int getWinner(int[] arr, int k) {
+        int n = arr.length;
+
+        if(k >= n){
+            int maxi = -1;
+            for(int num : arr){
+                maxi = Math.max(maxi, num);
+            }
+            return maxi;
+        }
+
+        int maxi = arr[0];
+        int consecutiveWins = 0;
+        for(int i = 1; i < n; i++){
+            if(maxi >= arr[i]){
+                consecutiveWins++;
+            } else {
+                maxi = arr[i];
+                consecutiveWins = 1;
+            }
+
+            if(consecutiveWins >= k) break;
+        }
+
+        return maxi;
+    }
+}
